f1e4b7889c6e585c26a04d60b6b10aa486abd0fb
Gabriel-Razvan <stefanescugabriel94@yahoo.com>
2024-07-06 21:38:43 +0300

    remoce strict mode

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -6,10 +6,7 @@ import './app/layout/styles.css'
 import { StoreContext, store } from './app/stores/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <StoreContext.Provider value={store}>
-      <App />
-    </StoreContext.Provider>
-   
-  </React.StrictMode>,
+  <StoreContext.Provider value={store}>
+    <App />
+  </StoreContext.Provider>
 )
