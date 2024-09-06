b8eb6b034e7287211f563d2d2a3b121ba7496b76
Gabriel-Razvan <stefanescugabriel94@yahoo.com>
2024-09-06 19:34:37 +0300

    end of section 12

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,97 @@
+using System.Security.Claims;
+using API.DTOs;
+using API.Services;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> m_UserManager;
+        private readonly TokenService m_tokenService;
+
+        public AccountController(UserManager<AppUser> userManager, TokenService tokenService)
+        {
+            m_UserManager = userManager;
+            this.m_tokenService = tokenService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await m_UserManager.FindByEmailAsync(loginDto.Email);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var result = await m_UserManager.CheckPasswordAsync(user, loginDto.Password);
+            if (result)
+            {
+                return CreateNewUserDto(user);
+            }
+
+            return Unauthorized();
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (await m_UserManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
+            {
+                return BadRequest("Username is already used");
+            }
+
+            if (await m_UserManager.Users.AnyAsync(x => x.Email == registerDto.Email))
+            {
+                return BadRequest("Email is already used");
+            }
+
+            var user = new AppUser
+            {
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                UserName = registerDto.Username,
+                Bio = ""
+            };
+
+            var result = await m_UserManager.CreateAsync(user, registerDto.Password);
+
+            if (result.Succeeded)
+            {
+                return CreateNewUserDto(user);
+            }
+
+            return BadRequest(result.Errors);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var user = await m_UserManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+
+            return CreateNewUserDto(user);
+        }
+
+        private UserDto CreateNewUserDto(AppUser user)
+        {
+            return new UserDto
+            {
+                DisplayName = user.DisplayName,
+                Image = null,
+                Token = m_tokenService.CreateToken(user),
+                Username = user.UserName,
+            };
+        }
+    }
+}
--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -1,13 +1,29 @@
 using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Persistence
 {
     public class Seed
     {
-        public static async Task SeedData(DataContext context)
+        public static async Task SeedData(DataContext context, UserManager<AppUser> userManager)
         {
+            if (!userManager.Users.Any())
+            {
+                var users = new List<AppUser>
+                {
+                    new AppUser{DisplayName = "Bob", UserName = "bob", Email="bob@test.com", Bio = ""},
+                    new AppUser{DisplayName = "Tom", UserName = "tom", Email="tom@test.com", Bio = ""},
+                    new AppUser{DisplayName = "Jane", UserName = "jane", Email="jabe@test.com", Bio = ""},
+                };
+
+                foreach (var user in users)
+                {
+                    await userManager.CreateAsync(user, "Password1!");
+                }
+            }
+
             if (context.Activities.Any()) return;
-            
+
             var activities = new List<Activity>
             {
                 new Activity
